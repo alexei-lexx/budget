ea0714b2cca6ee91ab1f0d4c26573c0897df28e6
alexei-lexx <alexei-lexx@users.noreply.github.com>
2025-06-08 20:18:36 +0200

    chore: update package.json to add ESLint and TypeScript ESLint configurations
    - Added ESLint and TypeScript ESLint as devDependencies.
    - Introduced lint and lint:fix scripts for code quality checks.
    - Updated the format script for consistency.
    - Created a new ESLint configuration file for TypeScript support.

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+//
+// See https://typescript-eslint.io/getting-started/
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+);
