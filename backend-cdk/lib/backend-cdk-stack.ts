7a1bf30d108966793b2e18150adb11857774888d
alexei-lexx <alexei-lexx@users.noreply.github.com>
2025-06-08 22:18:12 +0200

    feat: initialize backend-cdk project with TypeScript, Jest, and AWS CDK
    - Added package.json with necessary scripts and dependencies for TypeScript, Jest, and AWS CDK.
    - Created initial test file for backend-cdk with a sample test case for SQS Queue.
    - Configured TypeScript with tsconfig.json to enforce strict type checking and modern JavaScript features.

---
+++ backend-cdk/lib/backend-cdk-stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+export class BackendCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_LATEST, // Provide any supported Node.js runtime
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello CDK!'),
+          };
+        };
+      `),
+    });
+
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+      value: myFunctionUrl.url,
+    });
+  }
+}
