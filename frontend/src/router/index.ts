589f2fa9dd9bed3c33c26d76b1f2125020a1055d
alexei-lexx <alexei-lexx@users.noreply.github.com>
2025-06-21 21:11:39 +0200

    add authentication guard to accounts route
    - implements beforeEnter navigation guard
    - redirects unauthenticated users to dashboard

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from "vue-router";
 import Dashboard from "@/views/Dashboard.vue";
 import Accounts from "@/views/Accounts.vue";
+import { useAuth0 } from "@auth0/auth0-vue";
 
 const routes = [
   {
@@ -12,6 +13,14 @@ const routes = [
     path: "/accounts",
     name: "Accounts",
     component: Accounts,
+    beforeEnter: (_to: RouteLocationNormalized, _from: RouteLocationNormalized, next: NavigationGuardNext) => {
+      const { isAuthenticated } = useAuth0();
+      if (isAuthenticated.value) {
+        next();
+      } else {
+        next({ name: "Dashboard" });
+      }
+    },
   },
 ];
 
