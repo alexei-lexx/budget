67a383e21c89006136235667f0f50ad361daa753
alexei-lexx <alexei-lexx@users.noreply.github.com>
2025-11-03 23:29:42 +0100

    add newline for better readability

--- backend/src/resolvers/common_schemas.ts
+++ backend/src/resolvers/common_schemas.ts
@@ -17,6 +17,7 @@ export const categoryIdSchema = z.uuid({
 export const dateSchema = z
   .string()
   .regex(DATE_FORMAT_REGEX, DATE_FORMAT_ERROR_MESSAGE);
+
 export const descriptionSchema = z
   .string()
   .max(DESCRIPTION_MAX_LENGTH, DESCRIPTION_LENGTH_ERROR_MESSAGE)
