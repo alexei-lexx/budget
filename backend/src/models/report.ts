878b52e8d89304afda6b7a05a0313255664dbd94
alexei-lexx <alexei-lexx@users.noreply.github.com>
2026-02-28 20:03:46 +0100

    refactor ReportType to use const assertion for improved type safety

--- backend/src/models/report.ts
+++ backend/src/models/report.ts
@@ -1,4 +1,6 @@
-export enum ReportType {
-  INCOME = "INCOME",
-  EXPENSE = "EXPENSE",
-}
+export const ReportType = {
+  INCOME: "INCOME",
+  EXPENSE: "EXPENSE",
+} as const;
+
+export type ReportType = (typeof ReportType)[keyof typeof ReportType];
