887a8a17d0786b75c4ad231e522806c70e485328
Alexei Lexx <alexei-lexx@users.noreply.github.com>
2026-03-29 19:19:47 +0200

    extract dependency wiring into singleton container (#318)

---
+++ backend/src/utils/dependency-injection.ts
@@ -0,0 +1,10 @@
+export function createSingleton<T>(factory: () => T): () => T {
+  let instance: T | undefined;
+
+  return () => {
+    if (instance === undefined) {
+      instance = factory();
+    }
+    return instance;
+  };
+}
