248cc287abe8e352540611d04bf84f17d9919b50
alexei-lexx <alexei-lexx@users.noreply.github.com>
2026-04-09 13:36:30 +0200

    fix transfer id in fake

--- backend/src/utils/test-utils/repositories/transaction-repository-fakes.ts
+++ backend/src/utils/test-utils/repositories/transaction-repository-fakes.ts
@@ -6,16 +6,32 @@ import { toDateString } from "../../../types/date";
 export const fakeCreateTransactionInput = (
   overrides: Partial<CreateTransactionInput> = {},
 ): CreateTransactionInput => {
+  const type =
+    overrides.type ??
+    faker.helpers.arrayElement([
+      TransactionType.EXPENSE,
+      TransactionType.INCOME,
+      TransactionType.REFUND,
+      TransactionType.TRANSFER_IN,
+      TransactionType.TRANSFER_OUT,
+    ]);
+  const transferId = Object.hasOwn(overrides, "transferId")
+    ? overrides.transferId
+    : type === TransactionType.TRANSFER_IN ||
+        type === TransactionType.TRANSFER_OUT
+      ? faker.string.uuid()
+      : undefined;
+
   return {
     userId: faker.string.uuid(),
     accountId: faker.string.uuid(),
     categoryId: faker.string.uuid(),
-    type: TransactionType.EXPENSE,
+    type: type,
     amount: faker.number.float({ min: 1, max: 1000, fractionDigits: 2 }),
     currency: "USD",
     date: toDateString(faker.date.recent().toISOString().split("T")[0]),
     description: faker.finance.transactionDescription(),
-    transferId: undefined,
+    transferId,
     ...overrides,
   };
 };
